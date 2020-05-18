ace4b3519b231bdd5a7d7528d5e33706e4207b2c
ninjaboynaru <thiagoCord755@gmail.com>
2020-05-18 19:57:43 -0400

    Added custom native-base theme

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { StyleProvider, Spinner, H1 } from 'native-base';
 import getTheme from './native-base-theme/components';
-import theme from './native-base-theme/variables/material';
+import theme from './native-base-theme/variables/custom';
 import { ErrorDisplay, LoadingDisplay } from './app/metaComponents';
 import MainNavigation from './app/MainNavigation';
 import db from './app/db/db';
